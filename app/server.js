5267be1bd8a66cfb7f5306b7af6a1b7a05d8bcee
basilcea <ogbonnabasilcea@gmail.com>
2019-02-01 14:58:52 +0100

    ch(deploy): host on heroku [Finishes #163393885]

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use('/api/v1', router);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
