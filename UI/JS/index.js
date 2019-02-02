0bace3c2ef7d82253b0f03e3f7fe6a6a780e3697
basilcea <ogbonnabasilcea@gmail.com>
2019-02-02 15:27:09 +0100

    fix(responsiveness); responsiveness for smaller screens

--- UI/JS/index.js
+++ UI/JS/index.js
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+/** Get Divs by their Id*/
+=======
+<<<<<<< HEAD
+// Get Divs by their Id
+>>>>>>> 2ffd4e67b9b936273f22bf5cc07d345319d155cc
+const login = document.getElementById("login");
+const logon = document.getElementById("log");
+const signup = document.getElementById("signup");
+const signon = document.getElementById("sign");
+const reset =  document.getElementById("reset");
+const icon =  document.getElementById("hamburger");
+const loginLink = document.getElementById('loginLink')
+const backLink = document.getElementById('backLink')
+const signupLink = document.getElementById('signupLink')
+const resetLink = document.getElementById('resetLink')
+
+<<<<<<< HEAD
+
+=======
+=======
 /** Get Divs by their Id */
 const login = document.getElementById('login');
 const logon = document.getElementById('log');
@@ -9,8 +30,10 @@ const loginLink = document.getElementById('loginLink');
 const backLink = document.getElementById('backLink');
 const signupLink = document.getElementById('signupLink');
 const resetLink = document.getElementById('resetLink');
+>>>>>>> fix(endpoints): fix create party endpoint
 
 
+>>>>>>> 2ffd4e67b9b936273f22bf5cc07d345319d155cc
 /**  Get all buttons on the page */
 
 const buttons = document.querySelectorAll('button');
@@ -26,6 +49,21 @@ loginButton.className = 'button_active';
 signup.className = 'layout_none';
 reset.className = 'layout_none';
 
+<<<<<<< HEAD
+/**Trigger event when login button is clicked,
+    Display only login div, hide others.
+    change style of login button to active.*/
+
+loginButton.onclick =()=>{
+  location.href='home.html'
+	loginButton.className ="button_active";
+	signupButton.className ="button_login";
+	login.className = "layout_block";
+	signup.className = "layout_none";
+	reset.className = "layout_none";
+  icon.className ="background_icon";
+}
+=======
 /** Trigger event when login button is clicked,
     Display only login div, hide others.
     change style of login button to active. */
@@ -39,10 +77,21 @@ loginButton.onclick = () => {
   reset.className = 'layout_none';
   icon.className = 'background_icon';
 };
+>>>>>>> 2ffd4e67b9b936273f22bf5cc07d345319d155cc
 /** Trigger event when signup button is clicked.
     Display only signup div, hide others.
     change style of signup button to active. */
 
+<<<<<<< HEAD
+signupButton.onclick =()=>{
+	signupButton.className ="button_active";
+	loginButton.className ="button_login";
+	signup.className = "layout_block";
+	login.className = "layout_none";
+	reset.className = "layout_none";
+  icon.className ="background_icon"
+}
+=======
 signupButton.onclick = () => {
   signupButton.className = 'button_active';
   loginButton.className = 'button_login';
@@ -51,11 +100,63 @@ signupButton.onclick = () => {
   reset.className = 'layout_none';
   icon.className = 'background_icon';
 };
+>>>>>>> 2ffd4e67b9b936273f22bf5cc07d345319d155cc
 
 /** Trigger event when reset link is clicked.
     Display only login div, with rest password content.
     change style of signup button to active. */
 
+<<<<<<< HEAD
+resetLink.onclick =()=>{
+	signupButton.className ="button_login";
+	loginButton.className ="button_active";
+	signup.className = "layout_none";
+	login.className = "layout_none";
+	reset.className = "layout_block";
+  icon.className ="background_icon"
+}
+
+/** when go back linke is clicked, it should return you to login Div */
+backLink.onclick =()=>{
+  loginButton.onclick()
+}
+/** when already signed up link is clicked, it should return you to login Div */
+loginLink.onclick =()=>{
+  loginButton.onclick()
+}
+/** when signup link is clicked it should take you back to sign up div */
+signupLink.onclick =()=>{
+  signupButton.onclick()
+}
+
+/**------------- for small screen sizes -------------------*/
+
+/**when signon link is clicked link back to signup div */
+signon.onclick =()=>{
+  signupButton.onclick()
+}
+/** when lognon link is clicked link back to signup div */
+logon.onclick =()=>{
+  loginButton.onclick()
+}
+/** style signup and login links for small screen sizes */
+const smallLink = document.getElementsByClassName('nav_horizontal_small')
+
+icon.onclick =()=>{
+  if(icon.className === "background_icon"){
+    icon.className='background_icon1';
+    for (let i=0; i< smallLink.length ; i++ ){
+      smallLink[i].style.display='block'
+    }
+  }
+  else{
+    icon.className='background_icon'
+    for (let i=0; i< smallLink.length ; i++ ){
+      smallLink[i].style.display='none'
+    }
+  }
+}
+=======
 resetLink.onclick = () => {
   signupButton.className = 'button_login';
   loginButton.className = 'button_active';
@@ -104,6 +205,7 @@ icon.onclick = () => {
     }
   }
 };
+>>>>>>> 2ffd4e67b9b936273f22bf5cc07d345319d155cc
 /** Get all forms */
 const form1 = document.getElementById('form1');
 const form2 = document.getElementById('form2');
@@ -118,3 +220,7 @@ const reseted = document.getElementById('reseted');
 const signupdetails = form1.querySelectorAll('[required]');
 const enterdetails = form2.querySelectorAll('[required]');
 const resetdetails = form3.querySelectorAll('[required]');
+<<<<<<< HEAD
+
+=======
+>>>>>>> 2ffd4e67b9b936273f22bf5cc07d345319d155cc
