4623be07765d7bba7d4765ef1740713d1fd971be
basilcea <ogbonnabasilcea@gmail.com>
2019-02-02 06:49:05 +0100

    fix(endpoints): fix input requirements - implement object destructuring - create a default route and a catch all route - add more input conditions -use jsdoc for documentation [Fixes #163394504]

--- app/DS/models/office.js
+++ app/DS/models/office.js
@@ -1,29 +1,29 @@
-const offices =[
+const offices = [
   {
-    "id": 1 ,
-    "type": "Federal" ,
-    "name": "President-Nigeria" ,
-  },
-   {
-    "id": 2 ,
-    "type": "State" ,
-    "name": "Governor-Rivers" ,
+    id: 1,
+    type: 'Federal',
+    name: 'President-Nigeria'
   },
   {
-    "id": 3 ,
-    "type": "Legislative" ,
-    "name": "Senator-Akwa-Ibom-North" ,
+    id: 2,
+    type: 'State',
+    name: 'Governor-Rivers'
   },
-   {
-    "id": 4 ,
-    "type": "Legislative" ,
-    "name": "Representative-Anambara-Federal-Constituency-I" ,
+  {
+    id: 3,
+    type: 'Legislative',
+    name: 'Senator-Akwa-Ibom-North'
   },
-   {
-    "id": 5 ,
-    "type": "Local Government"  ,
-    "name": "Chairman-Kosofe" ,
+  {
+    id: 4,
+    type: 'Legislative',
+    name: 'Representative-Anambara-Federal-Constituency-I'
+  },
+  {
+    id: 5,
+    type: 'Local Government',
+    name: 'Chairman-Kosofe'
   }
-]
+];
 
-export default offices ;
+export default offices;
