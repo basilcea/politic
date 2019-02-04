61abd65251249ae3289cd656570a813e472a5f6b
basilcea <ogbonnabasilcea@gmail.com>
2019-02-04 01:32:19 +0100

    feat(endpoints): add authehtication to create user account endpoint [Finishes #163550930]

---
+++ app/middleware/auth.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import client from '../connect';
+import {redisClient} from '../connect'
+import 'dotenv';
+
+const auth = {
+    async checkToken (req,res,next){
+        const  token = req.headers['x-access-token']
+
+        if(!token) {
+            return res.status(400).send({ 'message': 'You need to Login' });
+        }
+        const invalid = (callback) => {
+            redisClient.lrange('token', 0,100, (err,result)=> {
+                return callback(result)
+            });
+         }
+        invalid((result)=>{
+            if (result.indexOf(token) > -1){
+                return res.status(400).json({'Message':'Invalid Token'})
+            }
+
+        })
+        try{
+            const decrypt = await jwt.verify(token, process.env.SECRET);
+            const getUser ='SELECT * FROM users WHERE userId= $1';
+
+            const {rows} = await client.query(getUser,[decrypt.userId]);
+            if (!rows[0]){
+                return res.status(400).json({
+                    'message':'Not accessible'
+                })
+            }
+            req.user = {
+                userId: decrypt.userId,
+                isAdmin: decrypt.isAdmin};
+            next();
+        }catch(error){
+            return res.status(400).send(error);
+        }
+    },
+
+}
+export default auth;
