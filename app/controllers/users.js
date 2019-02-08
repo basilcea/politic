6982a18071e45698f503cce5298ea95000f5c805
basilcea <ogbonnabasilcea@gmail.com>
2019-02-08 07:49:00 +0100

    fix

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -21,19 +21,20 @@ class userController {
   static async signup( req, res ) {
 
     // Sql query for inserting into user table
-    const createUser = `INSERT INTO users(firstname, lastname, othername, email,phoneNumber, passportURL,
-    password,registerAs,isAdmin)
+    const createUser = `INSERT INTO users(firstname, lastname, othername, email,phoneNumber, passportURL, password,registerAs,isAdmin)
     VALUES($1, $2, $3,$4, $5, $6 ,$7, $8, $9)`;
 
     // Sql query for  login users
     const loginUser = `SELECT * FROM users WHERE email = $1`;
 
     // hashpassword from helper class
-
     const getUser =`SELECT firstname from users`;
     const users= await pool.query(getUser);
     const getEmail=`SELECT email from users`;
     const email= await pool.query(getEmail);
+    const getphone=`SELECT phoneNumber from users`;
+    const phone = await pool.query(getphone);
+    const hashPassword = authHelper.hashPassword(req.body.password.trim())
     const values = [
         req.body.firstname,
         req.body.lastname,
@@ -54,7 +55,7 @@ class userController {
         });
       }
       /** validate first name to be letters alone */
-      if(!authHelper.isValidName(req.body.firstname)){
+      if(!authHelper.isValidName(req.body.firstname.trim())){
         return res.status(406).json({
           "status":406,
           "error":"First name must contain only letters"
@@ -74,53 +75,59 @@ class userController {
           "error":"Other name must be alphabets"
         })
       }
-      if (!req.body.email) {
+      if (!req.body.email.trim()) {
         return res.status(412).json({
           "status": 412,
           "error": 'Email is required'
         });
       }
 
-    if(authHelper.isUniqueEmail(req.body.email, email) !== null){
-     return res.status(422).json({
-       "status":422,
+    if(authHelper.isUniqueEmail(req.body.email.trim(), email) !== null){
+     return res.status(409).json({
+       "status":409,
        "error":'Email already exists'
      })
     }
-    if (!authHelper.isValidEmail(req.body.email)) {
-      return res.status(400).json({
-        "status":400,
+    if (!authHelper.isValidEmail(req.body.email.trim())) {
+      return res.status(406).json({
+        "status":406,
         "error": 'Please enter a valid email address'
       });
     }
-    if (!req.body.phoneNumber) {
+    if (!req.body.phoneNumber.trim()) {
       return res.status(412).json({
         "status": 412,
         "error" :'Phone Number is required'
       });
     }
-    if(!authHelper.isValidPhone(req.body.phoneNumber)){
+    if(!authHelper.isValidPhone(req.body.phoneNumber.trim())){
       return res.status(406).json({
         "status":406,
         "error":"invalid format.Must be 11 digits. Start with 0"
       })
     }
-    if(/^.*\.(jpg|JPG|PNG|png)$/.test(passportUrl) ===false){
+     if(authHelper.isUniquePhone(req.body.phoneNumber.trim(), phone)!==null){
+
+     return res.status(409).json({
+       "status":409,
+       "error":'Phone number already exists'
+     })
+    }
+    if(/^.*\.(jpg|JPG|PNG|png)$/.test(req.body.passportUrl.trim()) ===false){
       return res.status(406).json({
       "status": 406,
       "error": "Only .JPG or .PNG Accepted"
       })
     }
-    if (!req.body.password) {
+    if (!req.body.password.trim()) {
       return res.status(412).json({
         "status":412,
         "error": 'Password is required'
       });
     }
-    const hashPassword = authHelper.hashPassword(req.body.password);
-    if (!authHelper.isValidPassword(req.body.password)) {
-      return res.status(400).json({
-        "status":400,
+    if (!authHelper .isValidPassword(req.body.password.trim())) {
+      return res.status(406).json({
+        "status":406,
         "error": 'Please enter a valid password',
         "hint": 'Password must be between 6 and 15 characters long and must contain digits and letters'
       });
@@ -134,7 +141,7 @@ class userController {
     /** try and catch async block */
     try {
       await pool.query(createUser, values);
-      const { rows } = await pool.query(loginUser, [req.body.email]);
+      const { rows } = await pool.query(loginUser, [req.body.email.trim()]);
 
       // generate a user token for that user id
       const token = authHelper .generateToken(rows[0].id, rows[0].isadmin)
@@ -176,8 +183,8 @@ class userController {
     }
     // check if the email is valid
     if (!authHelper.isValidEmail(req.body.email)) {
-      return res.status(400).json({
-        "status": 400,
+      return res.status(406).json({
+        "status": 406,
         'error': 'Please enter a valid email address' });
     }
     const getUser = 'SELECT * FROM users WHERE email = $1';
--- app/seed/votes.js
+++ app/seed/votes.js
@@ -1,3 +1,5 @@
+
+
 const votes=[
   {
     "id":1,
